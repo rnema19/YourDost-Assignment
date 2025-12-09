1c75a68eaa5af599af14f28119b23fb94d8c8b5b
rnema19 <nemarishabh9@gmail.com>
2025-12-09 17:45:19 +0530

    ip commit

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express()
 let todoRoutes = require('./routes/todoRoutes')
+const ip = require('ip');
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
 app.use("/todos", todoRoutes)
+
+app.use("/", async (req,res) => {
+    var ipaddress = req.ip
+    res.send(ip)
+})
 
 app.listen(3000,()=>{
     console.log(`Server running on port 3000`);
